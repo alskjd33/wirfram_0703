af84ffbc095fe8da9c6249572e8ea4e19d661960
alskjd33 <alskjd33@gmail.com>
2024-07-04 11:53:39 +0900

    하단 쌤이 해준것 메모 자바로 슬라이드 이미지 움직이게하기 자바로 공지사항 클릭시 모달창 띄우기

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-$(document).ready(function(){
-    // tabmenu
-    $(function(){
-        $(".tabmenu > li > a") .click(function(){
-            // console.log(this);
-            $(this).parent().addClass("active").siblings().removeClass("active")
-        })
-    })
-})
+$(document).ready(function () {
+  // 서브메뉴전체가 보여지는 코드
+  //  $(".navi > li").mouseover(function(){
+  //   $(".submenu").stop().slideDown(500)
+  //  }).mouseout(function(){
+  //   $(".submenu").stop().slideUp(500)
+  //  })
+  // 네비에 해당하는 서브메뉴가 나오는코드
+  $(".navi > li").mouseover(function () {
+    // console.log(this);
+    $(this).find(".submenu").stop().slideDown(500);
+  });
+  $(".navi > li").mouseout(function () {
+    // console.log(this);
+    $(this).find(".submenu").stop().slideUp(500);
+  });
+  //   이미지 슬라이드 기능
+  setInterval(function(){
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({marginLeft: -1200});
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({marginLeft: -2400});
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({marginLeft: 0});
+    $(".slidelist").delay(2000);
+  })
+  // tabmenu
+  $(function () {
+    $(".tabmenu > li > a").click(function () {
+      // console.log(this);
+      $(this).parent().addClass("active").siblings().removeClass("active");
+    });
+  });
+  //모달기능
+  $(".popup").click(function(){
+    $(".modal").addClass("active")
+  })
+//   모달 닫기 기능
+$(".close").click(function(){
+    $(".modal").removeClass("active")
+  })
+});
